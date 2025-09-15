dd714063a138b6544b0f473275335d6805587bde
SylviaJ <923604368@qq.com>
2025-09-15 14:40:01 +0000

    minor

--- scheduler/run_server_sd2_cb.sh
+++ scheduler/run_server_sd2_cb.sh
@@ -1,3 +1,8 @@
 timestamp=$(date +"%Y%m%d_%H%M%S")
 echo "Server log will be saved to: $(pwd)/cb_sd2_server_log/log_sd2_${timestamp}.log"
+mkdir -p cb_sd2_server_log
 python -u server.py --config dist_config.yml --worker-max-batch-size 4 --scheduling-baseline "seq_length_balance" --pipeline-name SD2 > cb_sd2_server_log/log_sd2_${timestamp}.log 2>&1 &
+
+sleep 90
+echo "Server should start successfully"
+tail -n 20 cb_sd2_server_log/log_sd2_${timestamp}.log
